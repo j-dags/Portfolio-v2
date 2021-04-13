f3d07166be8b296e042fee3287bd421c8c071561
j-dags <jon.dagdagan@gmail.com>
2021-04-12 19:03:45 -0500

    Update connect message

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -48,11 +48,11 @@ const Contact = () => {
     <StyledContactSection id="contact" ref={revealContainer}>
       <h2 className="numbered-heading overline">What’s Next?</h2>
 
-      <h2 className="title">Get In Touch</h2>
+      <h2 className="title">Taking my talents to...</h2>
 
       <p>
-        Although I'm not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        I'm currently looking for my next career opportunity following the end of my fellowship.
+        Whether to discuss potential opportunities or just say hi, I'd love to connect!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
